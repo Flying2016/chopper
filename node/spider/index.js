a93b7e21be0f24bb5da1b04747f6092c1fe93baa
owen-carter <owenjiao@outlook.com>
2017-08-04 02:10:14 +0800

    update the update.sh

--- node/spider/index.js
+++ node/spider/index.js
@@ -1,3 +1,6 @@
-/**
- * Created by owen-carter on 2017/8/4.
- */
+#!/usr/bin/env node
+
+var inquirer = require('inquirer');
+inquirer.prompt([/* Pass your questions in here */]).then(function (answers) {
+    // Use user feedback for... whatever!!
+});
