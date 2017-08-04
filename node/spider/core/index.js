a28a77ffa2adecce87eb4dc39b13cbec0aeafc7d
owen-carter <owenjiao@outlook.com>
2017-08-04 21:58:19 +0800

    ll

--- node/spider/core/index.js
+++ node/spider/core/index.js
@@ -1,3 +1,21 @@
 /**
  * Created by owen-carter on 17/8/4.
  */
+
+
+class Spider {
+    constructor(url) {
+        this.url  = url;
+        this.pool = [];
+    }
+
+    init() {
+        return this;
+    }
+
+    run() {
+
+    }
+
+
+}
