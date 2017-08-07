a9e22123dfdfd7cd7ecf1e8c39e6ade7777a06a8
owen-carter <owenjiao@outlook.com>
2017-08-08 00:49:37 +0800

    delete useless simple this

--- node/spider/examples/components/base.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by owen on 2017/8/6.
- */
-class Worker {
-    constructor() {
-
-    }
-}
--- node/spider/index.js
+++ node/spider/index.js
@@ -1,73 +1,160 @@
-'use strict';
-var inquirer = require('inquirer');
+/**
+ * Created by owen on 2017/8/5.
+ */
+const fs = require('fs');
+const request = require("request");
+const cheerio = require("cheerio");
+const mkdirp = require('mkdirp');
+const path = require('path');
+const http = require("http");
+const log4js = require('log4js');
+const async = require('async');
+const cluster = require('cluster');
+const numCpus = require('os').cpus().length;
+const child_process = require('child_process');
+const x = child_process.fork(`${__dirname}/worker/parse.js`);
 
-var questions = [
-    {
-        type   : 'input',
-        name   : 'first_name',
-        message: 'What\'s your first name'
-    },
-    {
-        type   : 'input',
-        name   : 'last_name',
-        message: 'What\'s your last name',
-        default: function () {
-            return 'Doe';
-        }
-    },
-    {
-        type    : 'input',
-        name    : 'phone',
-        message : 'What\'s your phone number',
-        validate: function (value) {
-            var pass = value.match(/^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i);
-            if (pass) {
-                return true;
+
+class Spider {
+    constructor(seedUrl, storePath = './images/') {
+        this.seedUrl = seedUrl;
+        this.storePath = storePath;
+        this.urlPool = [];
+        this.ruleMap = {};
+    }
+
+    /***
+     * 可以初始化应用
+     * 启动各个进程
+     * @returns {Spider}
+     */
+    init() {
+        this.urlPool.push(this.seedUrl);
+        mkdirp(this.storePath, function (err) {
+            if (err) {
+                console.log(err);
             }
+        });
+        return this;
+    }
 
-            return 'Please enter a valid phone number';
+    /***
+     * scheduler ？添加url
+     * @param url
+     */
+    addUrl(url) {
+        this.urlPool.push(url);
+    }
+
+
+    /***
+     * 可以做成一个进程
+     * @param url
+     */
+    fetch(url) {
+        //发送请求
+        request(url, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                this.logger.log(`download ${url} is successful!`);
+                this.parse(url, body);
+            } else {
+                this.logger.log(error);
+            }
+        });
+    }
+
+    /***
+     * 将这个做成一个进程
+     * @param url
+     * @param html
+     */
+    parse(url, html) {
+        this.logger.log('开始解析！');
+        let $ = cheerio.load(html);
+        let instance = this;
+        $('a').each(function () {
+            let href = $(this).attr('href');
+            if (href.split('/').pop().indexOf('.') !== -1) {
+                instance.logger.log(`加入爬行队列 ${href}`);
+                instance.addUrl(href)
+            }
+        });
+        $('img').each(function () {
+            let src = $(this).attr('src');
+            let filename = src.split('/').pop();
+            instance.logger.log(`加入下载文件： ${src}`);
+            instance.download(src, instance.storePath, filename);
+        });
+    }
+
+    store(_path, _filename, data) {
+        fs.writeFile(`${_path}${_filename}`, data, "binary", function (err) {
+            if (err) {
+                console.log(`store ${_filename} fail`);
+            }
+            console.log(`store ${_filename} success`);
+        });
+    }
+
+    /***
+     * 一个下载方法可以作为工具类
+     * 同时还应该补充许多
+     * @param url
+     * @param _path
+     * @param filename
+     */
+    download(url, _path, filename) {
+        this.logger.log(`开始下载 ${url}`);
+        try {
+            http.get(url, (res) => {
+                let imgData = "";
+                res.setEncoding("binary")
+                    .on("data", (chunk) => {
+                        imgData += chunk;
+                    })
+                    .on("end", () => {
+                        this.store(this.storePath, filename, imgData)
+                    });
+            });
+
+        } catch (e) {
+            throw new Error(10, "download error")
         }
     }
-];
 
-inquirer.prompt(questions).then(function (answers) {
-    console.log(JSON.stringify(answers, null, '  '));
-});
-
-inquirer.prompt([
-    {
-        type    : 'checkbox',
-        message : '请选择需要从页面中爬取的元素！',
-        name    : 'target',
-        choices : [
-            new inquirer.Separator(' = 图片 = '),
-            {
-                name: 'png'
-            },
-            {
-                name: 'jpg'
-            },
-            {
-                name: 'gif'
-            },
-            new inquirer.Separator(' = 链接 = '),
-            {
-                name: 'link'
-            },
-            {
-                name: 'ed2k'
-            },
-            {
-                name: 'magnet'
-            }
-        ],
-        validate: (answer) => {
-            if (answer.length < 1) {
-                return '必须选择一种类型！';
-            }
-            return true;
+    /***
+     * 添加规则
+     * @param ruleName
+     * @param ruleFn
+     * @returns {Spider}
+     */
+    register(ruleName, ruleFn) {
+        if (!ruleName || !ruleFn) {
+            this.logger.error('注册规则失败');
+            throw "注册规则失败";
         }
+        this.ruleMap[ruleName] = ruleFn;
+        return this;
     }
-]).then(function (answers) {
 
+
+    run() {
+        while (this.urlPool.length) {
+            this.fetch(this.urlPool.shift())
+        }
+        this.logger.log('pool is empty,so exit');
+    }
+
+}
+
+
+(new Spider('http://www.mzitu.com/zipai/'))
+    .init()
+    .run();
+
+
+x.on('message', (data) => {
+    console.log('PARENT got message:', data);
 });
+
+x.send({hello: 'world'});
