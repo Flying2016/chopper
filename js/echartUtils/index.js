0438bd694f8381762cecb3898d83f0eaea699ce8
owen-carter <owenjiao@outlook.com>
2017-08-09 09:50:35 +0800

    add svn script

---
+++ js/echartUtils/index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by owen-carter on 17/8/8.
+ */
+
+const ChartUtils = function loginFn() {
+    this.chart = null;
+};
+
+
+ChartUtils.prototype.update = function (cb) {
+    let opt = this.chart.getOption(option);
+    opt     = cb(opt)
+    this.chart.setOption(opt)
+
+};
+
+ChartUtils.prototype.init = function (id) {
+    this.chart  = echarts.init(document.getElementById(id));
+    this.option = {}
+};
+
+ChartUtils().prototype.draw = function (option) {
+    this.chart.setOption(option)
+};
+
