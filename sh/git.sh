df3bd1ab5d4a1a82708696074002034f8c67c68b
owen-carter <owenjiao@outlook.com>
2017-08-09 22:15:10 +0800

    add 91

--- sh/git.sh
+++ sh/git.sh
@@ -6,6 +6,11 @@ function deleteForever(){
     git filter-branch --force --index-filter "git rm --cached --ignore-unmatch ${pathYourWantToRemoveFile}" --prune-empty --tag-name-filter cat -- --all
     if [[ $? = 0 ]]; then
         git push origin master --force
+        # 清理空间
+        rm -rf .git/refs/original/
+        git reflog expire --expire=now --all
+        git gc --prune=now
+        git gc --aggressive --prune=now
     else
         printf "delete failed.."
     fi
