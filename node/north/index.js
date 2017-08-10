43d7653114d097f14255c43d5d473c7996678c0f
owen-carter <owenjiao@outlook.com>
2017-08-11 01:19:27 +0800

    proxy another  way

--- node/north/index.js
+++ node/north/index.js
@@ -1,23 +1,23 @@
 /**
  * Created by owen-carter on 17/8/9.
  */
-const fs      = require('fs');
+const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
-const log4js  = require('log4js');
+const log4js = require('log4js');
 
 
 log4js.configure({
     categories: {
         default: {
             appenders: ['spider', 'out'],
-            level    : 'info'
+            level: 'info'
         }
     },
-    appenders : {
-        out   : {type: 'stdout'},
+    appenders: {
+        out: {type: 'stdout'},
         spider: {
-            type    : 'file',
+            type: 'file',
             filename: './spider.log'
         }
     }
@@ -29,14 +29,20 @@ const logger = log4js.getLogger('spider');
 class Spider {
     constructor(seedUrl) {
         // 首页地址
-        this.seedUrl          = seedUrl;
-        this.filename         = './url.csv';
+        this.seedUrl = seedUrl;
+        this.filename = './url.csv';
         // 限制页面条数
-        this.pageNumberLimit  = 30000;
+        this.pageNumberLimit = 6;
         // 分页页面地址池
-        this.pageUrlList      = [];
+        this.pageUrlList = [];
         // 视频页页面地址池
         this.videoPageUrlList = [];
+        // 有效代理地址池
+        this.validProxyUrlPool = [];
+        // 代理页面分页页码
+        this.proxyPageNumber = 1;
+        // 代理页面分页页码上限
+        this.proxyPageNumberLimit = 200;
     }
 
     // touch file [./url.csv] if not exist /data
@@ -53,6 +59,95 @@ class Spider {
 
         });
         return this;
+    }
+
+
+    searchProxy(pageNumber = 1, cb) {
+        let url = `http://www.xicidaili.com/nn/${pageNumber}`;
+        let conf = {
+            url: url,
+            method: "GET",
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
+            }
+        };
+        logger.info(`start access proxy ${url}`);
+        request(conf, (error, response, body) => {
+            if (error) {
+                return;
+            }
+            cb(body)
+        });
+    }
+
+    parseProxy(html) {
+        let proxyUrlList = [];
+        let $ = cheerio.load(html);
+        let trList = $("#ip_list tr");
+        let proxy = {};
+        let tr;
+        let tdList;
+        let speed;
+        let connectTime;
+        for (let i = 1; i < trList.length; i++) {
+            tr = trList.eq(i);
+            tdList = tr.children("td");
+            proxy['ip'] = tdList.eq(1).text();
+            proxy['port'] = tdList.eq(2).text();
+            speed = tdList.eq(6).children("div").attr("title");
+            speed = speed.substring(0, speed.length - 1);
+            connectTime = tdList.eq(7).children("div").attr("title");
+            connectTime = connectTime.substring(0, connectTime.length - 1);
+            //用速度和连接时间筛选一轮
+            if (speed <= 5 && connectTime <= 1) {
+                logger.info(`find a nice proxy ${proxy['ip']}:${proxy['port']}`);
+                proxyUrlList.push(proxy);
+            }
+        }
+        // trList.each((index, element) => {
+        //     tdList = $(element).children('td');
+        //     proxy['ip'] = $(tdList).eq(1).text();
+        //     proxy['port'] =  $(tdList).eq(2).text();
+        //     console.dir(element)
+        //     speed =  $(tdList).eq(6).children("div").attr("title");
+        //     speed = speed.substring(0, speed.length - 1);
+        //     connectTime =  $(tdList).eq(7).children("div").attr("title");
+        //     connectTime = connectTime.substring(0, connectTime.length - 1);
+        //     if (speed <= 5 && connectTime <= 1) {
+        //         logger.info(`find a nice proxy ${proxy['url']}:${proxy['port']}`);
+        //         proxyUrlList.push(proxy);
+        //     }
+        // });
+        // 过滤地址
+        this.filterProxy(proxyUrlList);
+        if (this.proxyPageNumber < this.proxyPageNumberLimit) {
+            this.searchProxy(this.proxyPageNumber++, this.parseProxy.bind(this));
+        }
+    }
+
+    filterProxy(proxyUrlList) {
+        let url = "http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js";
+        for (let i = 0; i < proxyUrlList.length; i++) {
+            let proxy = proxyUrlList[i];
+            request({
+                url: url,
+                proxy: "http://" + proxy['ip'] + ":" + proxy['port'],
+                method: 'GET',
+                timeout: 20000  //20s没有返回则视为代理不行
+            }, (error, response, body) => {
+                let href;
+                if (error) {
+                    return;
+                }
+                if (response.statusCode == 200) {
+                    href = response.request['proxy']['href'];
+                    this.validProxyUrlPool.push(href);
+                    logger.info(`find a useful proxy : ${href}`);
+                } else {
+                    logger.info(`find a bad proxy ${response.request['proxy']['href']}`);
+                }
+            });
+        }
     }
 
     /***
@@ -81,7 +176,7 @@ class Spider {
     parseIndex(html) {
         logger.info('start parse the index page....');
         // logger.info(html);
-        let $    = cheerio.load(html);
+        let $ = cheerio.load(html);
         let href = $('#navcontainer li:nth-child(3) a').attr('href');
         logger.info(`index page href is ${href}`);
         return href;
@@ -131,9 +226,9 @@ class Spider {
 
     parseVideo(html) {
         let $, name, src;
-        $    = cheerio.load(html)
+        $ = cheerio.load(html)
         name = $('#viewvideo-title').text();
-        src  = $('#vid source').attr('src');
+        src = $('#vid source').attr('src');
         logger.info(`collected: ${name.replace(/\n/g, '')} -- ${src}`);
         this.store(name, src);
         this.fetch(this.videoPageUrlList.pop(), this.parseVideo.bind(this))
@@ -144,7 +239,7 @@ class Spider {
      */
     store(name, src) {
         name = Spider.chMop(name);
-        src  = Spider.chMop(src);
+        src = Spider.chMop(src);
         fs.appendFile(this.filename, `${name.replace(/\n/g, '')},${src}\n`, 'utf8', (err) => {
             if (err) throw err;
             logger.info(`${name.replace(/\n/g)},${src} appendTo ${this.filename} success!`);
@@ -167,14 +262,16 @@ class Spider {
      * 启动
      */
     run() {
-        this.fetch(this.seedUrl, (html) => {
-            let startUrl = this.parseIndex(html);
-            this.makePageUrlList(startUrl);
-            // 拿到所有页面了的url，但是不可以循环去消费掉，链接数太大，node hold不住
-            // 需要一种机制
-            // 访问一个page如果成功，那么就从新开始自己
-            this.fetch(this.pageUrlList.shift(), this.parsePage.bind(this))
-        })
+        this.searchProxy(this.proxyPageNumber, this.parseProxy.bind(this));
+
+        // this.fetch(this.seedUrl, (html) => {
+        //     let startUrl = this.parseIndex(html);
+        //     this.makePageUrlList(startUrl);
+        //     // 拿到所有页面了的url，但是不可以循环去消费掉，链接数太大，node hold不住
+        //     // 需要一种机制
+        //     // 访问一个page如果成功，那么就从新开始自己
+        //     this.fetch(this.pageUrlList.shift(), this.parsePage.bind(this))
+        // })
     }
 
 
