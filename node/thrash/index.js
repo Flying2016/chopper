c05986f3f6810b51029e68691891c0248d6c6aea
owen-carter <owenjiao@outlook.com>
2017-09-16 02:19:30 +0800

    all is down

---
+++ node/thrash/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by owen on 2017/9/16.
+ */
+
+const trashFile = require('./thrashfile');
---
+++ node/thrash/thrashfile.js
@@ -0,0 +1,19 @@
+/**
+ * Created by owen on 2017/9/16.
+ */
+class App {
+    constructor() {
+
+    }
+
+    parse() {
+
+    }
+
+
+    store() {
+
+    }
+
+}
+module.exports = App;
